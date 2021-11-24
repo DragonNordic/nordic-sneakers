c7c9a1fcd27dbfee6bea85d05549783e36d31f7a
Nordic <nordik.commerce@gmail.com>
2021-11-24 10:29:17 +0200

    Completed project

---
+++ js/main.js
@@ -0,0 +1,23 @@
+gsap.from('nav', {opacity:0, delay:1.6, duration:2.5, ease: "elastic.out(1,0.3)", y:-100});
+
+gsap.from('.left__search', {opacity:0, delay:1.8, duration:2.8, ease: "elastic.out(1,0.3)", y:-100});
+
+gsap.from('.left__brand', {opacity:0, delay:2.2, duration:3, ease: "elastic.out(1,0.3)", y:-100});
+
+gsap.from('.left__discount', {opacity:0, delay:1, scale:.1, duration:2.4, rotation:180,
+ ease: "expo.inOut", x:100});
+
+gsap.from('.bottom__shoes-cards', {opacity:0, delay:2.6, duration:3.5, ease: "elastic.out(1,0.3)", x:100});
+
+gsap.from('.selecselected__productted__product-image', {opacity:0, delay:3, duration:4, ease:"elastic.out(1,0.3)", y:100});
+
+gsap.from('.selected__product-size', {opacity:0, delay:3.5, duration:0.5, ease:"Expo.In", y:100});
+
+gsap.from('.circle1', {opacity:0, delay:4.5, scale:.1, duration:.7, ease:"expo.In"});
+
+gsap.from('.circle2', {opacity:0, delay:4.8, scale:.1, duration:.7, ease:"expo.In"});
+
+
+
+
+
